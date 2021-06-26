7084b170ae9500c86acea6400eff1107ccc8b60b
Irene Wang <lohas1107@gmail.com>
2021-06-26 16:45:12 +0800

    use annotation or controller layer to log time with aop

--- src/main/java/cc/secondbrain/demo/aspect/TimeLogAspect.java
+++ src/main/java/cc/secondbrain/demo/aspect/TimeLogAspect.java
@@ -4,13 +4,15 @@ import lombok.extern.log4j.Log4j2;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Log4j2
 @Aspect
 @Component
 public class TimeLogAspect {
-    @Around("@annotation(cc.secondbrain.demo.annotation.TimeLog)")
+
+    @Around("logTime() || controller()")
     public Object logAround(ProceedingJoinPoint pjp) throws Throwable {
         long startMillis = System.currentTimeMillis();
         Object proceed = pjp.proceed();
@@ -18,4 +20,13 @@ public class TimeLogAspect {
         log.info(String.format("Completed %s in %d ms", pjp.getSignature().toShortString(), executionTime));
         return proceed;
     }
+
+    @Pointcut("@annotation(cc.secondbrain.demo.annotation.TimeLog)")
+    public void logTime() {
+    }
+
+    @Pointcut("execution(* cc.secondbrain.demo.controller.*.*(..))")
+    public void controller() {
+    }
+
 }
